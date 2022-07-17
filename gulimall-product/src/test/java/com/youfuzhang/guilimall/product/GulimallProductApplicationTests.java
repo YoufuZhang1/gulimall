591ec007e71e27ad63dfdca9786779273cedcaf0
zyf <2733024658@qq.com>
2022-07-17 21:36:50 +0800

    项目初始化

---
+++ gulimall-product/src/test/java/com/youfuzhang/guilimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youfuzhang.guilimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
